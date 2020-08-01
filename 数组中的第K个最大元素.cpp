d810e16912925e27f804f99fdc64d6fc90964391
graceyangfan <yf1825617022@outlook.com>
2020-08-01 17:13:59 +0800

    Update 数组中的第K个最大元素.cpp

--- 数组中的第K个最大元素.cpp
+++ 数组中的第K个最大元素.cpp
@@ -1,3 +1,11 @@
+/***
+采用快排的思想，先使用rand()再swap相当于随机选择中轴值，可以避免划分的两部分极度不平衡
+为了避免l++和r--带来的l>r使用了双重的while,比较时采用严格的比较，改变内容分布后，两个指针要向中间移动，扩展两侧长度
+快速排序的特点为：
+平均时间 nlogn
+最坏为n**2
+排序不稳定,可能导致数组顺序发生变化
+***/
 class Solution {
 public:
     int quickSelect(vector<int> & a,int l,int r,int index)
